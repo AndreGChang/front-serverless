f43c5c2e92730b4edf9d35428d011872ed4bc796
AndreGChang <FireWitch2003@outlook.com>
2025-03-02 18:57:57 -0300

    ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // 🔥 Define a base corretamente
+  server: {
+    middlewareMode: true, // Garante que as rotas sejam servidas corretamente
+  }
 });
